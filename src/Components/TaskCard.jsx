e4f63e99b7636ec9314a8381706ec5c7307f558a
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-11-17 20:50:13 +0530

    task managment

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -5,6 +5,7 @@ const TaskCard = ({ index, name, status }) => {
   const [show, setShow] = useState(false);
   const [newName, setNewName] = useState(name);
 
+  // to change the status of a particular task :
   const handleStatus = () => {
     Store.dispatch({
       type: "update-status",
@@ -15,12 +16,14 @@ const TaskCard = ({ index, name, status }) => {
     });
   };
 
+  // to handle update for a task :
   const handleChange = (e) => {
     const value = e.target.value;
     setNewName(value);
   };
 
-  const handleUpdate = () => {
+  // to update a particular task :
+  const handleUpdate = (e) => {
     Store.dispatch({
       type: "update-task",
       payload: {
@@ -31,6 +34,7 @@ const TaskCard = ({ index, name, status }) => {
     });
   };
 
+  // to delete a particular task :
   const handleDelete = () => {
     Store.dispatch({
       type: "delete-task",
@@ -44,27 +48,19 @@ const TaskCard = ({ index, name, status }) => {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
         border: status ? "2px solid green" : "2px solid red",
-        borderRadius: "10px",
         padding: "10px",
         margin: "10px",
-        width: "300px", // Set a fixed width for each task card
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
       }}
     >
       <h1
         style={{
-          flex: 1,
           color: status ? "green" : "red",
-          marginRight: "10px",
         }}
       >
         {name}
       </h1>
-      <div style={{ display:"flex", flexDirection:"row" }}>
+      <div>
         <button
           style={{
             backgroundColor: status ? "#ff0000cc" : "#008000cc",
@@ -84,7 +80,9 @@ const TaskCard = ({ index, name, status }) => {
             borderRadius: "3px",
             cursor: "pointer",
           }}
-          onClick={() => setShow((pre) => !pre)}
+          onClick={() => {
+            setShow((pre) => !pre);
+          }}
         >
           Update
         </button>
@@ -106,7 +104,6 @@ const TaskCard = ({ index, name, status }) => {
             <label>Name </label>
             <input
               style={{
-                flex: 1,
                 padding: "5px",
                 margin: "5px",
                 borderRadius: "3px",
@@ -137,5 +134,3 @@ const TaskCard = ({ index, name, status }) => {
 };
 
 export { TaskCard };
-
-
